900c190c00f2c60f423f41bfc4195946afafea7e
Ahmed <sss.ahm7@gmail.com>
2023-03-24 02:50:01 +0500

    landing page responsive

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -9,13 +9,15 @@ const Home: React.FC = () => {
         <section className='landing-page'>
             <div style={{ backgroundColor: '#DADADC' }}>
                 <div className="row">
-                    <div className="col-md-6 my-auto">
-                        <div className='px-4 landing-text'>
+                    <div className="col-md-6 my-auto landing-text">
+                        <div className='px-4 main-heading'>
                             <h1>
                                 Save Time For More Important Things
                             </h1>
                         </div>
+                        <div className='bar-sm'>
                         <div className='text-bar mx-4 my-5'></div>
+                        </div>
                         <div className="sub-text px-4">
                             Let us take care of your laundry
                         </div>
@@ -29,22 +31,22 @@ const Home: React.FC = () => {
             </div>
             <div className="container">
                 <div className='row'>
-                    <div className="col-md-3">
+                    <div className="col-md-3 col-sm-6">
                         <div className="card">
                             card 1
                         </div>
                     </div>
-                    <div className="col-md-3">
+                    <div className="col-md-3 col-sm-6">
                         <div className="card">
                             card 1
                         </div>
                     </div>
-                    <div className="col-md-3">
+                    <div className="col-md-3 col-sm-6">
                         <div className="card">
                             card 1
                         </div>
                     </div>
-                    <div className="col-md-3">
+                    <div className="col-md-3 col-sm-6">
                         <div className="card">
                             card 1
                         </div>
