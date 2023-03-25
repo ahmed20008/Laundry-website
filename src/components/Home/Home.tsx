c6f29b13476f6e5d4bddacb4be464159b4d677ab
Ahmed <sss.ahm7@gmail.com>
2023-03-26 00:05:31 +0500

    plan cards

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -82,6 +82,131 @@ const Home: React.FC = () => {
                     </div>
                 </div>
             </section>
+            <section className='plan-price' style={{ background: '#464646' }}>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <div className="price-heading py-4">
+                                <h1 className='text-center'>Our Services are worth it!</h1>
+                            </div>
+                            <div className="d-flex justify-content-center">
+                                <div className="heading-underline"></div>
+                            </div>
+                            <div className="price-sub-heading py-4 text-center">
+                                <h4>PLAN AND PRICING</h4>
+                            </div>
+                        </div>
+                        <div className="col-md-4">
+                            <div className="card my-5" style={{ background: '#464646', border: '2px solid #fff', borderRadius: '13px' }}>
+                                <div className="plans-card">
+                                    <div className="plan-type">
+                                        <h3>STANDARD</h3>
+                                        <p className='pt-2'>Extra clothes cost N500 each.</p>
+                                    </div>
+                                    <div className="price pt-3">
+                                        <h1>N 5,000<span>/week</span></h1>
+                                    </div>
+                                    <div className="d-flex justify-content-center">
+                                        <div className="price-underline mt-3 mb-4"></div>
+                                    </div>
+                                    <div className="plan-points">
+                                        <ul>
+                                            <li>Wash and fold</li>
+                                            <li>5 pieces of clothes</li>
+                                            <li>Pick-up</li>
+                                        </ul>
+                                    </div>
+                                    <div className="subscribe-btn">
+                                        <button className='btn' style={{
+                                            padding: '5px 70px',
+                                            background: '#fff',
+                                            border: '1px solid #fff',
+                                            borderRadius: '3px',
+                                            fontSize: '24px',
+                                            color: '#35A7FF',
+                                            fontWeight: '500',
+                                        }}>
+                                            SUBSCRIBE
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-4">
+                            <div className="card my-5" style={{ background: '#fff', border: '2px solid #fff', borderRadius: '13px' }}>
+                                <div className="plans-card">
+                                    <div className="plan-type-premium">
+                                        <h3>STANDARD</h3>
+                                        <p className='pt-2'>Extra clothes cost N500 each.</p>
+                                    </div>
+                                    <div className="premium-price pt-3">
+                                        <h1>N 5,000<span>/week</span></h1>
+                                    </div>
+                                    <div className="d-flex justify-content-center">
+                                        <div className="premium-price-underline mt-3 mb-4"></div>
+                                    </div>
+                                    <div className="premium-plan-points">
+                                        <ul>
+                                            <li>Wash and fold</li>
+                                            <li>5 pieces of clothes</li>
+                                            <li>Pick-up</li>
+                                        </ul>
+                                    </div>
+                                    <div className="subscribe-btn">
+                                        <button className='btn' style={{
+                                            padding: '5px 70px',
+                                            background: '#35A7FF',
+                                            border: '1px solid #fff',
+                                            borderRadius: '3px',
+                                            fontSize: '24px',
+                                            color: '#fff',
+                                            fontWeight: '500',
+                                        }}>
+                                            SUBSCRIBE
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-4">
+                            <div className="card my-5" style={{ background: '#464646', border: '2px solid #fff', borderRadius: '13px' }}>
+                                <div className="plans-card">
+                                    <div className="plan-type">
+                                        <h3>PRO</h3>
+                                        <p className='pt-2'>Extra clothes cost N500 each.</p>
+                                    </div>
+                                    <div className="price pt-3">
+                                        <h1>N 12,500<span>/week</span></h1>
+                                    </div>
+                                    <div className="d-flex justify-content-center">
+                                        <div className="price-underline mt-3 mb-4"></div>
+                                    </div>
+                                    <div className="plan-points">
+                                        <ul>
+                                            <li>Dry Cleaning & Press</li>
+                                            <li>8 pieces of clothes</li>
+                                            <li>Pick-up & Delivery</li>
+                                        </ul>
+                                    </div>
+                                    <div className="subscribe-btn">
+                                        <button className='btn' style={{
+                                            padding: '5px 70px',
+                                            background: '#fff',
+                                            border: '1px solid #fff',
+                                            borderRadius: '3px',
+                                            fontSize: '24px',
+                                            color: '#35A7FF',
+                                            fontWeight: '500',
+                                        }}>
+                                            SUBSCRIBE
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
 
         </>
     );
